0628fac02f0615cc7bd48b72b80d76d7dbff1935
Thous-web <thousiyaalif2025@gmail.com>
2025-05-07 06:54:44 +0530

    changes

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PuzzleController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::post('/start', [PuzzleController::class, 'startPuzzle']);
 Route::post('/submit', [PuzzleController::class, 'submitWord']);
 Route::post('/end', [PuzzleController::class, 'endPuzzle']);
